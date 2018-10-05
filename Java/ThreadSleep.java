3aa0b69c0e01cb49341fec838e1e55fc9204fe74
Sunil Kumar Vilambi <svilambi@amphorainc.com>
2018-10-05 17:03:58 +0530

    update with parameter

--- Java/ThreadSleep.java
+++ Java/ThreadSleep.java
@@ -1,10 +1,8 @@
 public class ThreadSleep {
 	public static void main(String[] args) throws InterruptedException {
-		for (int i = 0; i < 1000000000; i++) {
+		for (int i = 0; i < Integer.parseInt(args[0]); i++) {
 			System.out.println(i);
 			Thread.sleep(1000);
 		}
-
 	}
-
 }
