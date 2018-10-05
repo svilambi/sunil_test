fe543e97a0f2615098f1f02954f5343caade2c01
Sunil Kumar Vilambi <svilambi@amphorainc.com>
2018-10-05 17:21:22 +0530

    adding another variable

--- Java/ThreadSleep.java
+++ Java/ThreadSleep.java
@@ -1,5 +1,6 @@
 public class ThreadSleep {
 	public static void main(String[] args) throws InterruptedException {
+		System.out.println(args[1]);
 		for (int i = 0; i < Integer.parseInt(args[0]); i++) {
 			System.out.println(i);
 			Thread.sleep(1000);
